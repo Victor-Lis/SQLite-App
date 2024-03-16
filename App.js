3b15ba98c0911d45064240df41a5215617a41b47
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-15 23:42:54 -0300

    v1.05
    - Criando função createTable

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('database.db');
+
+async function createTable() {
+  db.transaction((txn) => {
+    txn.executeSql(
+      `CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT,idade INTEGER)`, 
+      [],
+      (res) => console.log("Sucesso"),
+      (error) => {console.log(error)}
+    )
+  })
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title='Criar Tabela' onPress={createTable}/>
     </View>
   );
 }
@@ -15,6 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
