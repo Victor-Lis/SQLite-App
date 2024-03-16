b91baf3c2a20c81b7b8e2b41bb82d1c891d84016
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-15 23:17:08 -0300

    v1.03
    - Seguindo passos da documentação

--- App.js
+++ App.js
@@ -1,6 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as FileSystem from 'expo-file-system';
+import * as SQLite from 'expo-sqlite';
+import { Asset } from 'expo-asset';
+
+async function openDatabase(pathToDatabaseFile){
+  if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+    await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+  }
+  await FileSystem.downloadAsync(
+    Asset.fromModule(require(pathToDatabaseFile)).uri,
+    FileSystem.documentDirectory + 'SQLite/myDatabaseName.db'
+  );
+  return SQLite.openDatabase('myDatabaseName.db');
+}
+
+const db = SQLite.openDatabase('dbName', version);
+
+const readOnly = true;
+await db.transactionAsync(async tx => {
+  const result = await tx.executeSqlAsync('SELECT COUNT(*) FROM USERS', []);
+  console.log('Count:', result.rows[0]['COUNT(*)']);
+}, readOnly);
+
+await db.execAsync([{ sql: 'PRAGMA foreign_keys = ON;', args: [] }], false);
+console.log('Foreign keys turned on');
+
 export default function App() {
   return (
     <View style={styles.container}>
